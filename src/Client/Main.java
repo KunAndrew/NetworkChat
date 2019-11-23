a1e7d4cdba9c21f12d648d579d0f0c0a1b0a0df8
Andrew Kuncevich <kuncevich_andrei@mail.ru>
2019-11-23 13:47:03 +0300

    Simple server-client

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -4,16 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Сетевой чат");
+       // primaryStage.getIcons().add(new Image("C:\\Java\\Chat\\src\\client\\resources\\images\\stage_icon.png"));
+
         Parent root = FXMLLoader.load(getClass().getResource("resources/primary.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
+
     }
 
 
---
+++ src/server/auth/AuthService.java
@@ -0,0 +1,19 @@
+package server.auth;
+
+import org.jetbrains.annotations.Nullable;
+
+public interface AuthService {
+
+    void start();
+    void stop();
+
+    /**
+     *
+     * @param login
+     * @param pass
+     * @return nick or null
+     */
+    @Nullable
+    String getNickByLoginPass(String login, String pass);
+
+}
