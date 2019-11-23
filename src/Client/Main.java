49464522eb63ebf90a8b8341cee46e5af01dadbf
Andrew Kuncevich <kuncevich_andrei@mail.ru>
2019-11-23 15:52:34 +0300

    fix

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -7,14 +7,17 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Сетевой чат");
-       // primaryStage.getIcons().add(new Image("C:\\Java\\Chat\\src\\client\\resources\\images\\stage_icon.png"));
-
-        Parent root = FXMLLoader.load(getClass().getResource("resources/primary.fxml"));
+      //  primaryStage.getIcons().add(new Image("C:\\Java\\Chat\\src\\client\\resources\\images\\stage_icon.png"));
+        FXMLLoader loader = new FXMLLoader(new File("C:\\Java\\Chat\\src\\client\\primary.fxml").toURI().toURL());
+        Parent root = loader.load();
+      // Parent root = FXMLLoader.load(getClass().getResource("/primary.fxml"));
 
         primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
