ee36aee8f7776327cdaedba1c9a3eefa1b0bce31
Andrew Kuncevich <kuncevich_andrei@mail.ru>
2019-11-23 12:56:40 +0300

    init2

---
+++ src/Client/controller/message/IMessageService.java
@@ -0,0 +1,17 @@
+package Client.controller.message;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.Closeable;
+import java.io.IOException;
+
+public interface IMessageService extends Closeable {
+    void sendMessage(String message);
+
+    void processRetrievedMessage(String message);
+
+    @Override
+    default void close() throws IOException {
+        //Do nothing
+    }
+}
---
+++ src/Client/controller/message/ServerConnectionException.java
@@ -0,0 +1,8 @@
+package Client.controller.message;
+
+public class ServerConnectionException extends RuntimeException {
+
+    public ServerConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
